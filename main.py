e9bc83dc5df52c0d6337f0c3c5f41ef60cff40d2
kwihanganaemmy <62944743+kwihanganaemmy@users.noreply.github.com>
2023-06-16 12:52:52 +0000

    added template
    added template

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_word():
-  return "Hello, Emmanuel!"
+  return render_template('home.html')
 
 
 if __name__ == "__main__":
